b574ac3617433122919098194cab6f7ebc649e72
Michael Hall <mohall42@students.tntech.edu>
2019-09-08 15:26:26 -0400

    Finished about page

--- src/About.js
+++ src/About.js
@@ -12,18 +12,24 @@ export class About extends Component {
                 <Nav />
 
                 <Container fixed maxWidth="xl" className="about-container">
-                    <div className="learn-more">
-                        <h2>Learn More</h2>
+                    <div className="learn-more aboutItem">
+                        <h1>Learn More</h1>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                         Sunt minima commodi ut aperiam at iusto accusamus veniam officia,
-                        animi distinctio libero omnis sed quod tenetur amet quis non pariatur quasi!</p>
+                        animi distinctio libero omnis sed quod tenetur amet quis non pariatur quasi!
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores amet veritatis expedita eos blanditiis,
+                        maxime doloremque minima odit consequuntur sequi.
+                        Sit, placeat ut? Nihil maiores ad eligendi autem commodi alias.</p>
                     </div>
 
-                    <div className="our-mission">
-                        <h2>Our Mission</h2>
+                    <div className="our-mission aboutItem">
+                        <h1>Our Mission</h1>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                         Sunt minima commodi ut aperiam at iusto accusamus veniam officia,
-                        animi distinctio libero omnis sed quod tenetur amet quis non pariatur quasi!</p>
+                        animi distinctio libero omnis sed quod tenetur amet quis non pariatur quasi!
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores amet veritatis expedita eos blanditiis,
+                        maxime doloremque minima odit consequuntur sequi.
+                        Sit, placeat ut? Nihil maiores ad eligendi autem commodi alias.</p>
                     </div>
 
 
